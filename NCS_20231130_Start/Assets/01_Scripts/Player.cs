affb8ac4d444cb39a6b1aa48102c572f0bfafb4b
hansolKim <khs20113@gmail.com>
2023-12-01 15:45:48 +0900

    아스나 애니메이션 관련 추가
    리소스는 올리지않았고, 애니메이션 컨트롤러와 상체 아바타만 추가함

---
+++ NCS_20231130_Start/Assets/01_Scripts/Player.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 vec = Vector3.zero;
+    Animator anim;
+    float speed = 0;
+    [SerializeField]
+    float walkSpeed = 2.5f;
+    [SerializeField]
+    float runSpeed = 5f;
+
+    Rigidbody rigid;
+
+    public Transform handTr; //손에 총을 들려주기 위함
+    public Transform holsterTr; //홀스터에 총을 넣어두기 위함
+    public Transform PistolTr; //총 자체의 트랜스폼
+
+    Vector3 jumpVec = Vector3.zero;
+    Vector3 jumpVecOrg = new Vector3(0,10,0); //
+
+    bool IsDraw = false; //총 장착 여부? 처음엔 맨손 걍 서있으니까 isDraw false상태고, 총을 한번 장착하면 true가 될것임..
+
+    [SerializeField]
+    SkinnedMeshRenderer renderer; //옷을 입힐 대상. 
+    public Material[] bodies; //옷 시리즈
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        vec.x = Input.GetAxis("Horizontal");
+        vec.z = Input.GetAxis("Vertical");
+
+        if (vec.x ==0 && vec.z ==0) //가만히 있기
+        {            
+            speed = 0;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) //시프트를 누르고 있으면 걷기 출력
+            {
+                //anim.SetFloat("Speed", 2.5f);                
+                speed = walkSpeed;                
+            }
+            else //그게 아니면 뛰기 출력
+            {
+                //anim.SetFloat("Speed", 5);
+                speed = runSpeed;                
+            }
+            anim.SetFloat("PosX", vec.x);
+            anim.SetFloat("PosZ", vec.z);            
+        }
+
+        anim.SetFloat("Speed", speed);
+        transform.Translate(vec.normalized * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space)) //스페이스를 딱 누르기 시작한 그때
+        {
+            anim.SetBool("IsJump",true);
+            //rigid.AddForce(Vector3.up * 10 , ForceMode.Impulse); //힘을 한번 빡! 가함  
+            rigid.useGravity = false; //중력사용을 안하겠음. 
+            jumpVec = jumpVecOrg;//jumpVec == (0,10,0)
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            jumpVec.y = Mathf.Clamp(jumpVec.y - Time.deltaTime *10, 0, 10);
+            rigid.velocity = jumpVec;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space)) //스페이스에서 손을 뗐을때
+        {            
+            rigid.useGravity = true;
+            anim.SetBool("IsJump", false);
+        }
+
+
+        //총을 차는거 / 총해제랑 동일하게 탭을 쓸것임..
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (IsDraw) //이미 총을 장착중이니 해제할것임
+            {
+                IsDraw = false;
+                anim.SetTrigger("Holster");
+            }
+            else //총을 장착중이지 않아서 장착 진행할 것임.
+            {
+                IsDraw = true;
+                anim.SetTrigger("Draw");
+            }
+        }
+
+        //왼쪽 마우스 클릭하면 총을 발사할 것이고
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            anim.SetTrigger("Shoot");
+            if (IsDraw) //총을 장착중이라면~~~~ 총쏘기 진행..
+            {
+                Debug.Log("빵야");
+            }
+        }
+
+        //r키를 누르면 재장전 할것임
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetTrigger("Reload");
+            if (IsDraw)
+            {
+                Debug.Log("재장전하기");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //renderer.materials[0] = bodies[Random.Range(0, bodies.Length)];                       
+            renderer.material = bodies[Random.Range(0, bodies.Length)];
+        }
+    }
+
+    public void SetDraw(/*bool IsOn*/int val)     
+    {
+        if (val == 1)
+        {
+            //총을 손밑으로 달아줘야됨
+            PistolTr.SetParent(handTr);            
+        }
+        else if(val == -1)
+        {
+            //총을 홀스터 밑으로 달아줘야함.
+            PistolTr.SetParent(holsterTr);            
+        }
+        
+        // 부모의 위치에 나를 맞춤
+        PistolTr.localPosition = Vector3.zero;
+        PistolTr.localRotation = Quaternion.identity;
+    }
+}
